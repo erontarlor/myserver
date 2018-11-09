07d24a930a4e0797d2f89bef76aa982231009fe2
Carlos Schramer <eron.tar.lor@gmx.de>
2018-11-09 02:24:55 +0100

    Cleaned up unnecessary stuff. Fix OwnCloud market issue.

--- setup.sh
+++ setup.sh
@@ -32,7 +32,7 @@ then
 fi
 
 declare sshdConfig=/etc/ssh/sshd_config
-#declare ownCloudHtaccess=/var/www/owncloud/.htaccess
+
 
 getArgs()
 {
@@ -289,171 +289,12 @@ installDocker()
 }
 
 
-createSslConf()
-{
-  declare file=ssl.conf
-
-  call "echo '<VirtualHost *:\${APACHE_LISTEN}>' > $file"
-  call "echo 'ServerAdmin \${APACHE_SERVER_ADMIN}' >> $file"
-  call "echo 'DocumentRoot \${APACHE_DOCUMENT_ROOT}' >> $file"
-
-  call "echo 'ErrorLog \${APACHE_ERROR_LOG}' >> $file"
-  call "echo 'CustomLog \${APACHE_ACCESS_LOG} \${APACHE_LOG_FORMAT}' >> $file"
-
-  #   SSL Engine Switch:
-  #   Enable/Disable SSL for this virtual host.
-  call "echo 'SSLEngine on' >> $file"
-
-  #   A self-signed (snakeoil) certificate can be created by installing
-  #   the ssl-cert package. See
-  #   /usr/share/doc/apache2.2-common/README.Debian.gz for more info.
-  #   If both key and certificate are stored in the same file, only the
-  #   SSLCertificateFile directive is needed.
-  call "echo \"SSLCertificateFile    /etc/ssl/certs/${certificateDomain[0]}.pem\" >> $file"
-  call "echo \"SSLCertificateKeyFile /etc/ssl/private/${certificateDomain[0]}.key\" >> $file"
-
-  #   Server Certificate Chain:
-  #   Point SSLCertificateChainFile at a file containing the
-  #   concatenation of PEM encoded CA certificates which form the
-  #   certificate chain for the server certificate. Alternatively
-  #   the referenced file can be the same as SSLCertificateFile
-  #   when the CA certificates are directly appended to the server
-  #   certificate for convinience.
-  call "echo \"SSLCertificateChainFile /etc/ssl/certs/${certificateDomain[0]}.pem\" >> $file"
-
-  #   Certificate Authority (CA):
-  #   Set the CA certificate verification path where to find CA
-  #   certificates for client authentication or alternatively one
-  #   huge file containing all of them (file must be PEM encoded)
-  #   Note: Inside SSLCACertificatePath you need hash symlinks
-  #         to point to the certificate files. Use the provided
-  #         Makefile to update the hash symlinks after changes.
-  #SSLCACertificatePath /etc/ssl/certs/
-  #SSLCACertificateFile /etc/apache2/ssl.crt/ca-bundle.crt
-
-  #   Certificate Revocation Lists (CRL):
-  #   Set the CA revocation path where to find CA CRLs for client
-  #   authentication or alternatively one huge file containing all
-  #   of them (file must be PEM encoded)
-  #   Note: Inside SSLCARevocationPath you need hash symlinks
-  #         to point to the certificate files. Use the provided
-  #         Makefile to update the hash symlinks after changes.
-  #SSLCARevocationPath /etc/apache2/ssl.crl/
-  #SSLCARevocationFile /etc/apache2/ssl.crl/ca-bundle.crl
-
-  #   Client Authentication (Type):
-  #   Client certificate verification type and depth.  Types are
-  #   none, optional, require and optional_no_ca.  Depth is a
-  #   number which specifies how deeply to verify the certificate
-  #   issuer chain before deciding the certificate is not valid.
-  #SSLVerifyClient require
-  #SSLVerifyDepth  10
-
-  #   Access Control:
-  #   With SSLRequire you can do per-directory access control based
-  #   on arbitrary complex boolean expressions containing server
-  #   variable checks and other lookup directives.  The syntax is a
-  #   mixture between C and Perl.  See the mod_ssl documentation
-  #   for more details.
-  #<Location />
-  #SSLRequire (    %{SSL_CIPHER} !~ m/^(EXP|NULL)/ \
-  #            and %{SSL_CLIENT_S_DN_O} eq "Snake Oil, Ltd." \
-  #            and %{SSL_CLIENT_S_DN_OU} in {"Staff", "CA", "Dev"} \
-  #            and %{TIME_WDAY} >= 1 and %{TIME_WDAY} <= 5 \
-  #            and %{TIME_HOUR} >= 8 and %{TIME_HOUR} <= 20       ) \
-  #           or %{REMOTE_ADDR} =~ m/^192\.76\.162\.[0-9]+$/
-  #</Location>
-
-  #   SSL Engine Options:
-  #   Set various options for the SSL engine.
-  #   o FakeBasicAuth:
-  #     Translate the client X.509 into a Basic Authorisation.  This means that
-  #     the standard Auth/DBMAuth methods can be used for access control.  The
-  #     user name is the `one line' version of the client's X.509 certificate.
-  #     Note that no password is obtained from the user. Every entry in the user
-  #     file needs this password: `xxj31ZMTZzkVA'.
-  #   o ExportCertData:
-  #     This exports two additional environment variables: SSL_CLIENT_CERT and
-  #     SSL_SERVER_CERT. These contain the PEM-encoded certificates of the
-  #     server (always existing) and the client (only existing when client
-  #     authentication is used). This can be used to import the certificates
-  #     into CGI scripts.
-  #   o StdEnvVars:
-  #     This exports the standard SSL/TLS related `SSL_*' environment variables.
-  #     Per default this exportation is switched off for performance reasons,
-  #     because the extraction step is an expensive operation and is usually
-  #     useless for serving static content. So one usually enables the
-  #     exportation for CGI and SSI requests only.
-  #   o StrictRequire:
-  #     This denies access when "SSLRequireSSL" or "SSLRequire" applied even
-  #     under a "Satisfy any" situation, i.e. when it applies access is denied
-  #     and no other module can change it.
-  #   o OptRenegotiate:
-  #     This enables optimized SSL connection renegotiation handling when SSL
-  #     directives are used in per-directory context.
-  #SSLOptions +FakeBasicAuth +ExportCertData +StrictRequire
-  call "echo \"<FilesMatch \\\"\\\\.(cgi|shtml|phtml|php)$\\\">\" >> $file"
-  call "echo \"SSLOptions +StdEnvVars\" >> $file"
-  call "echo \"</FilesMatch>\" >> $file"
-  call "echo \"<Directory /usr/lib/cgi-bin>\" >> $file"
-  call "echo \"SSLOptions +StdEnvVars\" >> $file"
-  call "echo \"</Directory>\" >> $file"
-
-  #   SSL Protocol Adjustments:
-  #   The safe and default but still SSL/TLS standard compliant shutdown
-  #   approach is that mod_ssl sends the close notify alert but doesn't wait for
-  #   the close notify alert from client. When you need a different shutdown
-  #   approach you can use one of the following variables:
-  #   o ssl-unclean-shutdown:
-  #     This forces an unclean shutdown when the connection is closed, i.e. no
-  #     SSL close notify alert is send or allowed to received.  This violates
-  #     the SSL/TLS standard but is needed for some brain-dead browsers. Use
-  #     this when you receive I/O errors because of the standard approach where
-  #     mod_ssl sends the close notify alert.
-  #   o ssl-accurate-shutdown:
-  #     This forces an accurate shutdown when the connection is closed, i.e. a
-  #     SSL close notify alert is send and mod_ssl waits for the close notify
-  #     alert of the client. This is 100% SSL/TLS standard compliant, but in
-  #     practice often causes hanging connections with brain-dead browsers. Use
-  #     this only for browsers where you know that their SSL implementation
-  #     works correctly.
-  #   Notice: Most problems of broken clients are also related to the HTTP
-  #   keep-alive facility, so you usually additionally want to disable
-  #   keep-alive for those clients, too. Use variable "nokeepalive" for this.
-  #   Similarly, one has to force some clients to use HTTP/1.0 to workaround
-  #   their broken HTTP/1.1 implementation. Use variables "downgrade-1.0" and
-  #   "force-response-1.0" for this.
-  call "echo \"BrowserMatch \\\"MSIE [2-6]\\\" \\\\\" >> $file"
-  call "echo \"nokeepalive ssl-unclean-shutdown \\\\\" >> $file"
-  call "echo \"downgrade-1.0 force-response-1.0\" >> $file"
-  # MSIE 7 and newer should be able to use keepalive
-  call "echo \"BrowserMatch \\\"MSIE [17-9]\\\" ssl-unclean-shutdown\" >> $file"
-
-  call "echo '<Directory \${APACHE_DOCUMENT_ROOT}>' >> $file"
-  call "echo \"AllowOverride All\" >> $file"
-  call "echo \"Options -Indexes +FollowSymlinks\" >> $file"
-  call "echo \"</Directory>\" >> $file"
-  call "echo \"</VirtualHost>\" >> $file"
-}
-
-
 declare passwordSha1
 calculatePasswordSha1()
 {
   declare array
   call "array=(\$( echo -n \"$1\" | sha1sum ))"
   passwordSha1=${array[0]}
-}
-
-
-createOwnCloudConf()
-{
-  declare file=owncloud.conf
-
-  call "echo \"Alias /owncloud /var/www/owncloud\" > $file"
-  call "echo \"<Directory /var/www/owncloud/>\" >> $file"
-  call "echo \"  AllowOverride All\" >> $file"
-  call "echo \"</Directory>\" >> $file"
 }
 
 
@@ -479,27 +320,8 @@ installOwnCloud()
   sleep 10
   calculatePasswordSha1 "$ownCloudPassword"
   call "docker-compose exec owncloud mysql -h db -powncloud -P 3306 -u owncloud -D owncloud -e \"update oc_users set password='$passwordSha1' where uid='admin';\""
-#  createOwnCloudConf
-#  call 'docker cp owncloud.conf $(docker ps -q -f name=owncloud_1):/etc/apache2/conf-enabled'
-#  call "rm owncloud.conf"
-#  call "docker-compose exec owncloud sed -e 's/\(RewriteBase \).*/\1\/owncloud\//' -i $ownCloudHtaccess"
-
-#  call "docker cp /etc/ssl/certs/${certificateDomain[0]}.pem "'$(docker ps -q -f name=owncloud_1)'":/etc/ssl/certs"
-#  call "docker cp /etc/ssl/private/${certificateDomain[0]}.key "'$(docker ps -q -f name=owncloud_1)'":/etc/ssl/private"
-#  call "docker-compose exec owncloud chown root:root /etc/ssl/certs/${certificateDomain[0]}.pem"
-#  call "docker-compose exec owncloud chmod 644 /etc/ssl/certs/${certificateDomain[0]}.pem"
-#  call "docker-compose exec owncloud chown root:ssl-cert /etc/ssl/private/${certificateDomain[0]}.key"
-#  call "docker-compose exec owncloud chmod 640 /etc/ssl/private/${certificateDomain[0]}.key"
-#  call "docker-compose exec owncloud ln -s /etc/apache2/mods-available/socache_shmcb.load /etc/apache2/mods-enabled"
-#  call "docker-compose exec owncloud ln -s /etc/apache2/mods-available/ssl.conf /etc/apache2/mods-enabled"
-#  call "docker-compose exec owncloud ln -s /etc/apache2/mods-available/ssl.load /etc/apache2/mods-enabled"
-#  createSslConf
-#  call 'docker cp ssl.conf $(docker ps -q -f name=owncloud_1):/etc/apache2/conf-enabled'
-#  call "rm ssl.conf"
-#  call "docker-compose exec owncloud chown root:root /etc/apache2/conf-enabled/ssl.conf"
-#  call "docker-compose exec owncloud chmod 644 /etc/apache2/conf-enabled/ssl.conf"
-#  call "docker-compose restart owncloud"
-
+  call "docker-compose exec owncloud rm /mnt/data/files/files_external/rootcerts.crt"
+  call "docker-compose restart owncloud"
   call "cd .."
 }
 
@@ -523,10 +345,6 @@ createSite()
   call "echo \"ServerAlias www.$1\" >> $file"
   call "echo \"ServerAdmin webmaster@localhost\" >> $file"
 
-  #call "echo \"SSLEngine on\" >> $file"
-  #call "echo \"SSLProxyEngine On\" >> $file"
-  #call "echo \"ProxyPreserveHost On\" >> $file"
-  #call "echo \"ProxyRequests Off\" >> $file"
   call "echo \"ProxyPass / http://localhost:8080/\" >> $file"
   call "echo \"ProxyPassReverse / http://localhost:8080/\" >> $file"
 
@@ -539,7 +357,7 @@ createSite()
 
   #   SSL Engine Switch:
   #   Enable/Disable SSL for this virtual host.
-#  call "echo \"SSLEngine on\" >> $file"
+  #SSLEngine on
 
   #   A self-signed (snakeoil) certificate can be created by installing
   #   the ssl-cert package. See
@@ -701,7 +519,7 @@ configureApache()
 
 disableSshRootLogin()
 {
-  call "sed -e 's/^\( *PermitRootLogin .*\)$/#\1/' $sshdConfig > $sshdConfig.new && mv $sshdConfig.new $sshdConfig"
+  call "sed -e 's/^\( *PermitRootLogin .*\)$/#\1/' -i $sshdConfig"
   call "echo \"PermitRootLogin no\" >> $sshdConfig"
 }
 
@@ -711,7 +529,7 @@ changeSshPort()
   askInteger "Which port do you want to use for SSH" $sshdPort
   sshdPort=$integer
   echo "Changing SSL port to $sshdPort..."
-  call "sed -e 's/^\( *Port .*$\)/#\1/' $sshdConfig > $sshdConfig.new && mv $sshdConfig.new $sshdConfig"
+  call "sed -e 's/^\( *Port .*$\)/#\1/' -i $sshdConfig"
   call "echo \"Port $sshdPort\" >> $sshdConfig"
 }
 
