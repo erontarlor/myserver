ec37f43289d082b43ed6faafbcd2261fa091931c
erontarlor <44510259+erontarlor@users.noreply.github.com>
2024-09-01 01:24:25 +0200

    Fix grep parameters.

--- rootaccessoff.sh
+++ rootaccessoff.sh
@@ -2,7 +2,7 @@
 
 # Switch off SSH root access
 declare sshdConfig=/etc/ssh/sshd_config
-if grep -e '^ *PermitRootLogin' $sshdConfig; then
+if grep -q -e '^ *PermitRootLogin' $sshdConfig; then
   sed -e 's/^\( *PermitRootLogin\).*$/\1 no/' -i $sshdConfig
 else
   echo "PermitRootLogin no" >> $sshdConfig
