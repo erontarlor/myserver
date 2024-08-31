156371111fcb84c4ea9eebe3957f7d26d5559703
erontarlor <44510259+erontarlor@users.noreply.github.com>
2024-09-01 01:14:50 +0200

    Create rootaccesson.sh

---
+++ rootaccesson.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Switch on SSH root access
+declare sshdConfig=/etc/ssh/sshd_config
+if grep -e '^ *PermitRootLogin' $sshdConfig; then
+  sed -e 's/^\( *PermitRootLogin\).*$/\1 yes/' -i $sshdConfig
+else
+  echo "PermitRootLogin yes" >> $sshdConfig
+fi
+systemctl restart ssh.service
