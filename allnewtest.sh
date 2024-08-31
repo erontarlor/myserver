29e2096195fea318f5ff71a5df37f45a5267863d
erontarlor <44510259+erontarlor@users.noreply.github.com>
2024-08-31 13:56:03 +0200

    Allow specifying Nextcloud version number.

--- allnewtest.sh
+++ allnewtest.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+# Redo setting up the server without deleting data.
+# Use self signed test certificates.
+# If version number is specified as first command line argument, install this version of Nextcloud.
 docker-compose -f ~/nextcloud/docker-compose.yml kill
 docker-compose -f ~/nextcloud/docker-compose.yml rm -f
 curl https://github.com/erontarlor/myserver/raw/master/setup.sh --output ~/setup.sh --location
 chmod u+x ~/setup.sh
+if [ "$1" != "" ]; then
+  sed -i -e "s/image: nextcloud/image: nextcloud:$1/" ~/setup.sh
+fi
 ~/setup.sh -auto -testcertificates
