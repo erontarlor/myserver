7ef70d68f08254657676b8b63a284310c8878183
erontarlor <44510259+erontarlor@users.noreply.github.com>
2024-09-01 01:24:45 +0200

    Fix grep parameters.

--- rootaccesson.sh
+++ rootaccesson.sh
@@ -2,7 +2,7 @@
 
 # Switch on SSH root access
 declare sshdConfig=/etc/ssh/sshd_config
-if grep -e '^ *PermitRootLogin' $sshdConfig; then
+if grep -q -e '^ *PermitRootLogin' $sshdConfig; then
   sed -e 's/^\( *PermitRootLogin\).*$/\1 yes/' -i $sshdConfig
 else
   echo "PermitRootLogin yes" >> $sshdConfig
